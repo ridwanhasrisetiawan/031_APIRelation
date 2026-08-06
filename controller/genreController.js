f0ee7f34b52526c9ccfab3df126944363f6cfa49
ridwanhasrisetiawan <ridwanhasrisetiawan@gmail.com>
2026-08-06 13:26:20 +0700

    add: implement getAll function to retrieve all genres

--- controller/genreController.js
+++ controller/genreController.js
@@ -2,7 +2,17 @@ const db = require("../models");
 
 const Genre = db.Genre;
 
+async function getAll(req, res) {
+  try {
+    const genres = await Genre.findAll();
 
+    return res.status(200).json(genres);
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message
+    });
+  }
+}
 
 
 
