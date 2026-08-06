38be919753a0952e77277899311abd72333da2ed
ridwanhasrisetiawan <ridwanhasrisetiawan@gmail.com>
2026-08-06 13:18:58 +0700

    initialize express server and connect to database

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-
+const express = require("express");
+const app = express();
+const PORT = 3000;
 const genreRoute = require('./routes/genreRoute');
+const connectDatabase = require('./config/db');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", require("./routes/api"));
 app.use("/api/genres", genreRoute);
 
+async function startServer() {
+    await connectDatabase();
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
+}
+
+startServer();
