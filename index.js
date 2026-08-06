858e3b3799a860671e762d5315f1cefbca9ef882
ridwanhasrisetiawan <ridwanhasrisetiawan@gmail.com>
2026-08-06 12:49:50 +0700

    connect database

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const express = require("express");
-const app = express();
-const PORT = 3000;
+
+const connectDatabase = require('./config/db');
 
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
+async function startServer() {
+    await connectDatabase();
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
+}
+
+startServer();
